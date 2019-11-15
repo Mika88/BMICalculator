7337dadecf04fd5d759f7cf45963e2ff9be2573a
Mika <mikaspivak@gmail.com>
2019-11-14 20:43:15 -0500

    Create a Console class. Add a scanner field and a getInput method to get the input from the user

---
+++ src/com/company/Console.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Console {
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static double getInput(String input){
+    return scanner.nextDouble();
+  }
+}
