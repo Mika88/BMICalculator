7868476161726afce661a1f0d19cd77aed0516a9
Mika <mikaspivak@gmail.com>
2019-11-14 21:13:07 -0500

    Change parameter name in getInput to prompt, since we are prompting the user to enter an input

--- src/com/company/Console.java
+++ src/com/company/Console.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Console {
   private static Scanner scanner = new Scanner(System.in);
 
-  public static double getInput(String input){
+  public static double getInput(String prompt){
     return scanner.nextDouble();
   }
 }
