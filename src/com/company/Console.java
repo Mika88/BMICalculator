118a366f5a284fbb6b44675951d52df0661e3d28
Mika <mikaspivak@gmail.com>
2019-11-24 18:17:37 -0500

    Refactor getInput() method in Console class to have a breaking condition if the input is negative

--- src/com/company/Console.java
+++ src/com/company/Console.java
@@ -3,9 +3,18 @@ package com.company;
 import java.util.Scanner;
 
 public class Console {
-  private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
-  public static double getInput(String prompt){
-    return scanner.nextDouble();
-  }
-}
+    public static double getInput(String prompt) {
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if (value < 0)
+                break;
+            System.out.println("Enter a value bigger than 0");
+            }
+           return value;
+        }
+    }
+
