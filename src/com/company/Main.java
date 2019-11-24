d37c0cce793236093e2c6f7bf0e7b648978fe9c7
Mika <mikaspivak@gmail.com>
2019-11-24 18:33:13 -0500

    Declare weight and height variables to prompt the user to insert his information

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+      double weight = Console.getInput("Enter Weight in Kg: ");
+      double height = Console.getInput("Enter Height in Meters: ");
     }
 }
