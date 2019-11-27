263fa3287c5276a2406a586c36d73644342f522f
Mika <mikaspivak@gmail.com>
2019-11-27 13:45:22 -0500

    Declare a bmi variable and assign it to BMICalculator instance. call the calculateBMI() method on it.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
       float weight = (float) Console.getInput("Enter Weight in Kg: ");
       float height = (float) Console.getInput("Enter Height in Meters: ");
+
       var bmi = new BMICalculator(height, weight);
+      bmi.calculateBMI();
     }
 }
