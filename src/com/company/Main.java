17240498960a619dce59341ae05bde5784677427
Mika <mikaspivak@gmail.com>
2019-12-08 20:31:01 -0500

    Refactor input arguments passed to height and weight variables in main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-      float weight =  Console.getInput("Enter Weight in Kg: ");
-      float height =  Console.getInput("Enter Height in Meters: ");
+      float weight =  Console.getInput("Enter Your Weight in Kg: ");
+      float height =  Console.getInput("Enter Your Height in Meters: ");
 
       var bmi = new BMICalculator(height, weight);
       Console.printBMI(bmi);
